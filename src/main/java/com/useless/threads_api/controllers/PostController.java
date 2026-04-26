7452ba3b2cbd08f5eff6d18c52a5e95bd8d10c6a
Useless8000B <adrielluiz73@gmail.com>
2026-04-26 19:56:55 -0300

    feat: add endpoint to retrieve posts by user ID

--- src/main/java/com/useless/threads_api/controllers/PostController.java
+++ src/main/java/com/useless/threads_api/controllers/PostController.java
@@ -12,6 +12,8 @@ import org.springframework.web.bind.annotation.RestController;
 import com.useless.threads_api.models.PostModel;
 import com.useless.threads_api.repositories.PostRepository;
 
+import jakarta.servlet.http.HttpServletRequest;
+
 @RestController
 @RequestMapping("/api/posts")
 public class PostController {
@@ -22,14 +24,23 @@ public class PostController {
 	}
 
 	@PostMapping
-	public ResponseEntity<String> createPost(@RequestBody PostModel post) {
+	public ResponseEntity<String> createPost(@RequestBody PostModel post) throws Exception {
 		String id = _postRepository.savePost(post);
 		return ResponseEntity.status(201).body(id);
 	}
 
 	@GetMapping
-	public ResponseEntity<List<PostModel>> listPosts() {
+	public ResponseEntity<List<PostModel>> listPosts() throws Exception {
 		List<PostModel> posts = _postRepository.getAllPosts();
+		return ResponseEntity.ok(posts);
+	}
+
+	@GetMapping("/me")
+	public ResponseEntity<List<PostModel>> getMyPosts(HttpServletRequest request) throws Exception {
+		String uid = (String) request.getAttribute("uid");
+
+		List<PostModel> posts = _postRepository.findByUuid(uid);
+
 		return ResponseEntity.ok(posts);
 	}
 }
