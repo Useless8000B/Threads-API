d6353c7835e2c560ab18b3a37fdd22d08d38e7d3
Useless8000B <adrielluiz73@gmail.com>
2026-04-27 17:00:46 -0300

    Fix authentication

--- src/main/java/com/useless/threads_api/config/FirebaseTokenFilter.java
+++ src/main/java/com/useless/threads_api/config/FirebaseTokenFilter.java
@@ -1,7 +1,10 @@
 package com.useless.threads_api.config;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import com.google.firebase.auth.FirebaseAuth;
@@ -23,6 +26,12 @@ public class FirebaseTokenFilter extends OncePerRequestFilter {
 					try {
 						FirebaseToken decodedToken = FirebaseAuth.getInstance().verifyIdToken(token);
 						request.setAttribute("uid", decodedToken.getUid());
+						UsernamePasswordAuthenticationToken auth = new UsernamePasswordAuthenticationToken(
+							token,
+							null,
+							new ArrayList<>()
+						);
+						SecurityContextHolder.getContext().setAuthentication(auth);
 					} catch (Exception e) {}
 				}
 		filterChain.doFilter(request, response);
