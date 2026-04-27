3b4a6fb523a4608faf1ebb6817e79183d1050a7b
Useless8000B <adrielluiz73@gmail.com>
2026-04-27 16:24:48 -0300

    Added new exception

---
+++ src/main/java/com/useless/threads_api/exceptions/UnauthorizedException.java
@@ -0,0 +1,7 @@
+package com.useless.threads_api.exceptions;
+
+public class UnauthorizedException extends RuntimeException{
+	public UnauthorizedException(String message) {
+		super(message);
+	}
+}
