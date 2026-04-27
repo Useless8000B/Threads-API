e31711b884eb582197d18327080a038eaffa1f02
Useless8000B <adrielluiz73@gmail.com>
2026-04-26 21:58:30 -0300

    Fix 403 error

--- src/main/java/com/useless/threads_api/config/SecurityConfig.java
+++ src/main/java/com/useless/threads_api/config/SecurityConfig.java
@@ -16,7 +16,7 @@ public class SecurityConfig {
             .csrf(csrf -> csrf.disable())
             .authorizeHttpRequests(auth -> auth
                 .requestMatchers("/api/posts/**").authenticated()
-                .anyRequest().authenticated()
+                .anyRequest()
             );
         
         return http.build();
