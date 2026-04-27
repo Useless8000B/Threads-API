a42300859d4d1dc4c7fdff0c3ffad02b93cd678e
Useless8000B <adrielluiz73@gmail.com>
2026-04-27 12:32:58 -0300

    feat: add FirebaseAuth bean to Firebase configuration

--- src/main/java/com/useless/threads_api/config/FirebaseConfig.java
+++ src/main/java/com/useless/threads_api/config/FirebaseConfig.java
@@ -4,6 +4,7 @@ import com.google.auth.oauth2.GoogleCredentials;
 import com.google.cloud.firestore.Firestore;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.cloud.FirestoreClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -37,5 +38,10 @@ public class FirebaseConfig {
 		}
 
 		return FirestoreClient.getFirestore();
+	}
+
+	@Bean
+	public FirebaseAuth firebaseAuth() throws IOException {
+		return FirebaseAuth.getInstance();
 	}
 }
