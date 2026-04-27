15b8e9bef00ed0fdd000e9e5ba118d266532646b
Useless8000B <adrielluiz73@gmail.com>
2026-04-27 15:44:58 -0300

    Remove extra space

--- src/main/java/com/useless/threads_api/config/FirebaseTokenFilter.java
+++ src/main/java/com/useless/threads_api/config/FirebaseTokenFilter.java
@@ -12,7 +12,7 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-public class FirebaseTokenFilter  extends OncePerRequestFilter {
+public class FirebaseTokenFilter extends OncePerRequestFilter {
 	@Override
 	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
 			throws ServletException, IOException {
