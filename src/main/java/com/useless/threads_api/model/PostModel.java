200574c030e445f41d68590a6c287d7374e93015
Useless8000B <adrielluiz73@gmail.com>
2026-04-29 13:18:04 -0300

    Update PostModel
    Now the model includes a json property and a profile pic

--- src/main/java/com/useless/threads_api/model/PostModel.java
+++ src/main/java/com/useless/threads_api/model/PostModel.java
@@ -2,6 +2,7 @@ package com.useless.threads_api.model;
 
 import java.util.Date;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.google.cloud.firestore.annotation.DocumentId;
 import com.google.cloud.firestore.annotation.ServerTimestamp;
 
@@ -14,11 +15,13 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class PostModel {
 	@DocumentId
+	@JsonProperty("id")
 	private String id;
 
 	private String uid;
 	private String username;
 	private String text;
+	private String profilePic;
 
 	@ServerTimestamp
 	private Date createdAt;
