a8b9d9fc6a0e0402ab0ca464c50eb939b405100c
Useless8000B <adrielluiz73@gmail.com>
2026-04-29 13:18:23 -0300

    Add method to delete posts

--- src/main/java/com/useless/threads_api/controllers/PostController.java
+++ src/main/java/com/useless/threads_api/controllers/PostController.java
@@ -3,12 +3,16 @@ package com.useless.threads_api.controllers;
 import java.util.List;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.useless.threads_api.exceptions.ForbiddenException;
+import com.useless.threads_api.exceptions.NotFoundException;
 import com.useless.threads_api.exceptions.UnauthorizedException;
 import com.useless.threads_api.model.PostModel;
 import com.useless.threads_api.repository.PostRepository;
@@ -50,5 +54,27 @@ public class PostController {
 		List<PostModel> posts = _postRepository.findByUid(uid);
 
 		return ResponseEntity.ok(posts);
+	}
+
+	@DeleteMapping("/{id}")
+	public ResponseEntity<String> deletePost(@PathVariable String id, HttpServletRequest request) throws Exception {
+		String uid = (String) request.getAttribute("uid");
+
+		if (uid == null) {
+			throw new UnauthorizedException("User not authenticated");
+		}
+
+		PostModel post = _postRepository.findbyId(id);
+
+		if (post == null) {
+			throw new NotFoundException("Post not found!");
+		}
+
+		if (!post.getUid().equals(uid)) {
+			throw new ForbiddenException("You don't have permisson to delete this post");
+		}
+
+		_postRepository.removePost(id);
+		return ResponseEntity.ok("Post removed");
 	}
 }
